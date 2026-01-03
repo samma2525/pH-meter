ed25b826665899482dc150675789eff309b3a3a3
Samma <sammawalidaly@gmail.com>
2026-01-04 00:31:29 +0200

    Some updates to make it work better

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let voltValue;
+let Eo;
 let R = 8.314; // J/(mol·K)
 let F = 96485; // C/mol
 let T = 298; // K  
@@ -7,16 +8,17 @@ let Ln10 = Math.log(10);
 
 
 document.getElementById("mySubmit").onclick = function() {
-    voltValue = document.getElementById("voltInput").value;
-    document.getElementById("pHtext").textContent = calculatePH(voltValue);
+    voltValue = parseFloat(document.getElementById("voltInput").value);
+    Eo = parseFloat(document.getElementById("Eo").value);
+    document.getElementById("pHtext").textContent = calculatePH(voltValue, Eo);
 }
 
-function calculatePH(voltValue) {
-    if (voltValue > 0.828 || voltValue < 0) {
+function calculatePH(voltValue, Eo) {
+    if (voltValue < 0) {
         document.getElementById("Note").textContent = "Please enter a possible voltage value, the range is between 0.828 & 0.";
     } else {
-        document.getElementById("Note").textContent = "Please enter a possible voltage value, the range is between 0.828 & 0.";
-        let pH = (voltValue * F) / (2.303 * R * T);
+        document.getElementById("Note").textContent = "";
+        let pH = Eo - voltValue / 0.05916;
         return pH.toFixed(2);
     }
 }
